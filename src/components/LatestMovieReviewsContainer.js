f314e8a92bf9bcdb44b4ecbf3d169dc0ffbf3e94
Vlad <VladDaImpaler@gmail.com>
2020-12-22 11:04:33 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,45 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            reviews: []
+        };
+    }
+    componentDidMount() {
+        fetch(URL)
+        .then(response => response.json())
+        .then(reviewData => {
+            this.setState({reviews: reviewData.results})
+            return console.log(reviewData.results)
+        }
+            )
+    }
+    
+    renderMovies = () => {
+        return this.state.reviews.map(movie =>{
+            return (
+                <div className="review">
+                    <h3>{ movie.display_title }</h3>
+                    <h4>By: { movie.byline }</h4>
+                    <p>{ movie.summary_short }</p>
+                    <a href={movie.link.url}>{ movie.link.suggested_link_text }</a>
+                    <br></br>----------------------------------------------------------------------
+                    <br></br>
+                </div>
+            )
+        })
+    }
+    render(){
+
+        return(
+            <div className='latest-movie-reviews'>
+                { this.renderMovies() }
+            </div>
+        )
+    }
+}
+export default LatestMovieReviewsContainer
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React, { Component } from 'react'
+
+class MovieReviews extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default MovieReviews
